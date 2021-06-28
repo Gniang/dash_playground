65c372904e432eaf68ceb2e25f2381d4bb0ef557
kazuse.kashihara <kazuse.kashihara@kkmiura.jp>
2021-06-28 18:39:34 +0900

    interactive dash

--- main.py
+++ main.py
@@ -23,11 +23,26 @@ app.layout = html.Div(
     [
         dcc.Interval(
             id='interval-component',
-            interval=1*1000, # in milliseconds
+            interval=3*1000, # in milliseconds
             n_intervals=0
         ),
-        dcc.Input(id='img_val', type='text'),
-        html.Div(id='last_img', style='width:300px; height:400px;'),
+        html.Div(
+            [
+                html.Label('interval'),
+                dcc.Input(id='interval_sec', type='text', value='3'),
+            ]
+        ),
+
+        html.Div([
+            html.Label('Image No'), 
+            html.Label(id='img_val'),
+        ]),
+
+        html.Div([
+            html.Img(id='last_img', style={'object-fit':'contain'}),
+            ],
+            style={'width':'300px', 'height':'400px'}
+        ),
 
         dcc.Graph(id='graph-with-slider'),
         dcc.Slider(
@@ -48,29 +63,34 @@ app.layout = html.Div(
 #     Output(component_id='last_img', component_property='children'),
 #     Input(component_id='img_val', component_property='value'),
 # )
-def update_img(input_value: str):
+def create_img_path(input_value: str):
     if input_value == None or not input_value.isdigit():
         return ""
 
-    fname = '{:04}.jpg'.format(int(input_value))
+    fname = '{:03}.jpg'.format(int(input_value))
     fpath = '{}/{}'.format(ddir, fname)
-    return html.Img(
-        id='img_{}'.format(fname),
-        src=app.get_asset_url(fpath),
-        # src=app.get_asset_url('cats/0001.jpg'),
-        style='object-fit=contain'
-    )
+    return app.get_asset_url(fpath)
+    # return html.Img(
+    #     id='img_{}'.format(fpath),
+    #     src=app.get_asset_url(fpath),
+    #     # src=app.get_asset_url('cats/0001.jpg'),
+    #     style={'object-fit':'contain'}
+    # )
 
 class static:
     cnt = 0
 
-@app.callback(Output('last_img', 'children'),
-            Input('interval-component', 'n_intervals'),              
-            Input('img_val', 'value'))
-def update_metrics(n, img_val):
-    static.cnt+=1
-    return update_img(str(static.cnt))
+@app.callback(Output('last_img', 'src'),
+            Input('img_val', 'children'))
+def update_metrics(img_val):
+    return create_img_path(img_val)
 
+@app.callback(Output('img_val', 'children'),
+            Input('interval-component', 'n_intervals'),              
+            )  
+def update_text(n):
+    static.cnt+=1
+    return str(static.cnt)
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
@@ -81,7 +101,9 @@ def update_figure(selected_year):
     fig = px.scatter(
         filterd_df,
         x="gdpPercap", y="lifeExp",
-        size="pop", color="continent", hover_name="country",
+        labels={'gdpPercap':"x", 'lifeExp':"y"},
+        # size="pos",
+        color="continent", hover_name="country",
         log_x=True, size_max=55)
 
     fig.update_layout(transition_duration=500)
